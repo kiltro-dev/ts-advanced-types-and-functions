1f979595b7812719cf220634e04708fd37baea79
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-19 15:26:03 -0300

    capacitor library and enums

---
+++ src/01-enum.ts
@@ -0,0 +1,12 @@
+enum ROLES {
+  ADMIN = 'admin',
+  SELLER = 'seller',
+  CUSTOMER = 'customer',
+}
+
+type User = {
+  username: string;
+  role: ROLES;
+};
+
+const user1: User = { username: 'Ignacio', role: ROLES.ADMIN };
