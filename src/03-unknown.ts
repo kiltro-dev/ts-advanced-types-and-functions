89541f45c6a2d3b1d052142b0f28ab4803f2e3df
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-19 16:05:30 -0300

    never

--- src/03-unknown.ts
+++ src/03-unknown.ts
@@ -3,3 +3,10 @@ let anyVar: any;
 anyVar = 34;
 
 const isNew: boolean = anyVar;
+
+let unknownVar: unknown;
+
+unknownVar = 34;
+
+const isNew2: boolean = true
+isNew2.valueOf();
---
+++ src/04-never.ts
@@ -0,0 +1,24 @@
+const withoutEnd = () => {
+  while (true) {
+    console.log('never stop');
+  }
+};
+
+const fail = (message: string) => { // TypeScript infiere que esta función se de tipo `never`
+  throw new Error(message)
+}
+
+const example = (input:unknown) => {
+  if(typeof input === 'string'){
+    return 'Es un string';
+  }
+  else if (Array.isArray(input)){
+    return 'Es un array';
+  }
+  return fail('Not Match'); // Lanzamos un error
+}
+
+console.log(example('Hola')) //'Es un string'
+console.log(example([1,1,1,1])) // 'Es un array'
+console.log(example(1212)) // error: Uncaught Error: Not Match
+console.log(example('Hola después del fail')) // NUNCA SE EJECUTA, porque se lanzó un error previamente
