f297c09fcb5d45fb1c4eee96b38f58b50ed74458
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-19 18:07:19 -0300

    readonly

--- src/app/prroducts/product.service.ts
+++ src/app/prroducts/product.service.ts
@@ -1,7 +1,9 @@
-import { Product } from "./product.model";
+import { Product } from './product.model';
 
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = 'dadasdas';
+  // data.createdAt = new Date();
   products.push(data);
 };
